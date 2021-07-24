72d7ff8d45c4188b6022be617f2947a4b7f2cc98
Phamminh202 <73330638+Phamminh202@users.noreply.github.com>
2021-07-24 23:49:36 +0700

    Tark1PHP

---
+++ Tark01/EMP_DETAILS.php
@@ -0,0 +1,13 @@
+<?php
+error_reporting(0);
+$A=$_POST["empid"];
+$B=$_POST["name"];
+$C=$_POST["dept"];
+$D=$_POST["email"];
+echo "YOUR PERSONAL DETAILS";
+echo "<br><br>";
+echo "EMPID: $A <BR>";
+echo "NAME: $B <BR>";
+echo "DEPARTMENT NAME: $C <BR>";
+echo "EMAIL: $D <BR>";
+?>
---
+++ Tark01/case_ins.php
@@ -0,0 +1,7 @@
+<?php
+define("NAME","Michael",true);
+echo NAME;
+echo "<br>";
+echo name;
+echo "<br>";
+?>
